5a7c8a23a590ba46fae5e431ca129993ebbf9df7
CPallares96 <urielesc@uninorte.edu.co>
2020-05-25 19:52:54 -0500

    .

--- mainLeaf.m
+++ mainLeaf.m
@@ -1,173 +1,194 @@
 %ACTUALIZADO 3.07PM 13/05/2020
 %Primera capa %
-XY=imread('666.png');
+XY=imread('mayo25.png');
 XY=imresize(XY,[128 128]);
 %imagen=rgb2gray(XY);
 imagen=(XY);
 filtro=[0,7,0;9,9,1;1,0,0];%filtro invertido *****1
 imagenVec=reshape(imagen,1,[]);
-filtroVec=reshape(filtro,1,[]);
-[imageSize,asd]=size(imagen);
-[x,y]=size(filtro);
-rango=(imageSize-x)+1;
+% filtroVec=reshape(filtro,1,[]);
+% [imageSize,asd]=size(imagen);
+% [x,y]=size(filtro);
+% rango=(imageSize-x)+1;
+% 
+% %MATRIZ DE PESOS CAPA 1
+% 
+% vecIndexI=1:2;
+% vecIndexJ=1:2;
+% vecValue=1:2;
+% 
+% i=1;
+% index=1;
+% con=0;
+% 
+% while(i<(imageSize-2)*imageSize)
+%     
+%     con=con+1;
+%     j=i+imageSize;
+%     k=j+imageSize;
+% 
+%     vecIndexI(index)=i;
+%     vecIndexJ(index)=con;
+%     vecValue(index)=filtroVec(1);
+%     
+%     vecIndexI(index+1)=i+1;
+%     vecIndexJ(index+1)=con;
+%     vecValue(index+1)=filtroVec(2);
+% 
+%     vecIndexI(index+2)=i+2;
+%     vecIndexJ(index+2)=con;
+%     vecValue(index+2)=filtroVec(3);
+%     
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+%     vecIndexI(index+3)=j;
+%     vecIndexJ(index+3)=con;
+%     vecValue(index+3)=filtroVec(4);
+% 
+%     vecIndexI(index+4)=j+1;
+%     vecIndexJ(index+4)=con;
+%     vecValue(index+4)=filtroVec(5);
+% 
+%     vecIndexI(index+5)=j+2;
+%     vecIndexJ(index+5)=con;
+%     vecValue(index+5)=filtroVec(6);
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+%     vecIndexI(index+6)=k;
+%     vecIndexJ(index+6)=con;
+%     vecValue(index+6)=filtroVec(7);
+% 
+%     vecIndexI(index+7)=k+1;
+%     vecIndexJ(index+7)=con;
+%     vecValue(index+7)=filtroVec(8);
+% 
+%     vecIndexI(index+8)=k+2;
+%     vecIndexJ(index+8)=con;
+%     vecValue(index+8)=filtroVec(9);
+%     index=index+9;
+% 
+%     
+%     
+%     if(rem(i+2,imageSize)==0)
+%         i=i+3;
+%     else
+%         i=i+1;
+%     end
+% end
+% 
+% w4=sparse(vecIndexI,vecIndexJ,vecValue); %DEFINICIÓN MATRIZ DE PESOS
+% a5=sparse(im2double(imagenVec));
+% argRelu=sparse(a5*w4); %CONVOLUCION(IMAGEN,FILTRO)
+% n=1; %Aumenta los valores para el input
+% inputCapa2=0.30*(reshape(n*reLu(argRelu),rango,[])); %PENDIENTE EL 0.30 QUE RESALTA COSAS
+% %imshow(reshape(full(inputCapa2),510,[]));
+% 
+% 
+% %MATRIZ DE PESOS CAPA 2
+% 
+% [imageSizeCapa2,asd]=size(inputCapa2);
+% %filtro2=[-1,0,1;2,0,0;-1,-2,-1]; %El mas firme en convergencia
+% filtro2=[1,-1,2;-1,1,3;-1,4,1];
+% filtroVec2=reshape(filtro2,1,[]);
+% 
+% [x,y]=size(filtro2);
+% rango=(imageSizeCapa2-x)+1;
+% 
+% 
+% vecIndexII=1:2;
+% vecIndexJJ=1:2;
+% vecValuee=1:2;
+% 
+% i=1;
+% index=1;
+% con=0;
+% 
+% while(i<(imageSizeCapa2-2)*imageSizeCapa2)
+%     
+%     con=con+1;
+%     j=i+imageSizeCapa2;
+%     k=j+imageSizeCapa2;
+% 
+%     
+%     vecIndexII(index)=i;
+%     vecIndexJJ(index)=con;
+%     vecValuee(index)=filtroVec2(1);
+%     
+%     vecIndexII(index+1)=i+1;
+%     vecIndexJJ(index+1)=con;
+%     vecValuee(index+1)=filtroVec2(2);
+% 
+%     vecIndexII(index+2)=i+2;
+%     vecIndexJJ(index+2)=con;
+%     vecValuee(index+2)=filtroVec2(3);
+%     
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+%     vecIndexII(index+3)=j;
+%     vecIndexJJ(index+3)=con;
+%     vecValuee(index+3)=filtroVec2(4);
+% 
+%     vecIndexII(index+4)=j+1;
+%     vecIndexJJ(index+4)=con;
+%     vecValuee(index+4)=filtroVec2(5);
+% 
+%     vecIndexII(index+5)=j+2;
+%     vecIndexJJ(index+5)=con;
+%     vecValuee(index+5)=filtroVec2(6);
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+%     vecIndexII(index+6)=k;
+%     vecIndexJJ(index+6)=con;
+%     vecValuee(index+6)=filtroVec2(7);
+% 
+%     vecIndexII(index+7)=k+1;
+%     vecIndexJJ(index+7)=con;
+%     vecValuee(index+7)=filtroVec2(8);
+% 
+%     vecIndexII(index+8)=k+2;
+%     vecIndexJJ(index+8)=con;
+%     vecValuee(index+8)=filtroVec2(9);
+%     index=index+9;
+% 
+%     
+%     
+%     if(rem(i+2,imageSizeCapa2)==0)
+%         i=i+3;
+%     else
+%         i=i+1;
+%     end
+% end
+% 
+% w3=sparse(vecIndexII,vecIndexJJ,vecValuee); %DEFINICIÓN MATRIZ DE PESOS
+% inputCapa2=reshape(inputCapa2,1,[]);
+% [sizet,tt]=size(inputCapa2);
+% a4=sparse(inputCapa2);
+% 
+% argRelu=sparse(a4*w3); %CONVOLUCION(IMAGEN,FILTRO)
+% n2=300; %Aumenta los valores para el input
+% a3=reLu(argRelu);
+% a3=a3*0.03;
+% %a3=sparse(n*funcionAct(argRelu));
+% imshow(reshape(full(a3),rango,[]));
 
-%MATRIZ DE PESOS CAPA 1
 
-vecIndexI=1:2;
-vecIndexJ=1:2;
-vecValue=1:2;
+z=load('w3.txt');
 
-i=1;
-index=1;
-con=0;
+row = z(:,1);
+col = z(:,2);
+v = z(:,3);
+w3 = sparse(row, col, v,15876,15376);
 
-while(i<(imageSize-2)*imageSize)
-    
-    con=con+1;
-    j=i+imageSize;
-    k=j+imageSize;
+z=load('w4.txt');
 
-    vecIndexI(index)=i;
-    vecIndexJ(index)=con;
-    vecValue(index)=filtroVec(1);
-    
-    vecIndexI(index+1)=i+1;
-    vecIndexJ(index+1)=con;
-    vecValue(index+1)=filtroVec(2);
+row = z(:,1);
+col = z(:,2);
+v = z(:,3);
+w4 = sparse(row, col, v,16384,15876);
 
-    vecIndexI(index+2)=i+2;
-    vecIndexJ(index+2)=con;
-    vecValue(index+2)=filtroVec(3);
-    
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
-    vecIndexI(index+3)=j;
-    vecIndexJ(index+3)=con;
-    vecValue(index+3)=filtroVec(4);
 
-    vecIndexI(index+4)=j+1;
-    vecIndexJ(index+4)=con;
-    vecValue(index+4)=filtroVec(5);
 
-    vecIndexI(index+5)=j+2;
-    vecIndexJ(index+5)=con;
-    vecValue(index+5)=filtroVec(6);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
-    vecIndexI(index+6)=k;
-    vecIndexJ(index+6)=con;
-    vecValue(index+6)=filtroVec(7);
-
-    vecIndexI(index+7)=k+1;
-    vecIndexJ(index+7)=con;
-    vecValue(index+7)=filtroVec(8);
-
-    vecIndexI(index+8)=k+2;
-    vecIndexJ(index+8)=con;
-    vecValue(index+8)=filtroVec(9);
-    index=index+9;
-
-    
-    
-    if(rem(i+2,imageSize)==0)
-        i=i+3;
-    else
-        i=i+1;
-    end
-end
-
-w4=sparse(vecIndexI,vecIndexJ,vecValue); %DEFINICIÓN MATRIZ DE PESOS
 a5=sparse(im2double(imagenVec));
-argRelu=sparse(a5*w4); %CONVOLUCION(IMAGEN,FILTRO)
-n=1; %Aumenta los valores para el input
-inputCapa2=0.30*(reshape(n*reLu(argRelu),rango,[])); %PENDIENTE EL 0.30 QUE RESALTA COSAS
-%imshow(reshape(full(inputCapa2),510,[]));
-
-
-%MATRIZ DE PESOS CAPA 2
-
-[imageSizeCapa2,asd]=size(inputCapa2);
-%filtro2=[-1,0,1;2,0,0;-1,-2,-1]; %El mas firme en convergencia
-filtro2=[1,-1,2;-1,1,3;-1,4,1];
-filtroVec2=reshape(filtro2,1,[]);
-
-[x,y]=size(filtro2);
-rango=(imageSizeCapa2-x)+1;
-
-
-vecIndexII=1:2;
-vecIndexJJ=1:2;
-vecValuee=1:2;
-
-i=1;
-index=1;
-con=0;
-
-while(i<(imageSizeCapa2-2)*imageSizeCapa2)
-    
-    con=con+1;
-    j=i+imageSizeCapa2;
-    k=j+imageSizeCapa2;
-
-    
-    vecIndexII(index)=i;
-    vecIndexJJ(index)=con;
-    vecValuee(index)=filtroVec2(1);
-    
-    vecIndexII(index+1)=i+1;
-    vecIndexJJ(index+1)=con;
-    vecValuee(index+1)=filtroVec2(2);
-
-    vecIndexII(index+2)=i+2;
-    vecIndexJJ(index+2)=con;
-    vecValuee(index+2)=filtroVec2(3);
-    
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
-    vecIndexII(index+3)=j;
-    vecIndexJJ(index+3)=con;
-    vecValuee(index+3)=filtroVec2(4);
-
-    vecIndexII(index+4)=j+1;
-    vecIndexJJ(index+4)=con;
-    vecValuee(index+4)=filtroVec2(5);
-
-    vecIndexII(index+5)=j+2;
-    vecIndexJJ(index+5)=con;
-    vecValuee(index+5)=filtroVec2(6);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
-    vecIndexII(index+6)=k;
-    vecIndexJJ(index+6)=con;
-    vecValuee(index+6)=filtroVec2(7);
-
-    vecIndexII(index+7)=k+1;
-    vecIndexJJ(index+7)=con;
-    vecValuee(index+7)=filtroVec2(8);
-
-    vecIndexII(index+8)=k+2;
-    vecIndexJJ(index+8)=con;
-    vecValuee(index+8)=filtroVec2(9);
-    index=index+9;
-
-    
-    
-    if(rem(i+2,imageSizeCapa2)==0)
-        i=i+3;
-    else
-        i=i+1;
-    end
-end
-
-w3=sparse(vecIndexII,vecIndexJJ,vecValuee); %DEFINICIÓN MATRIZ DE PESOS
-inputCapa2=reshape(inputCapa2,1,[]);
-[sizet,tt]=size(inputCapa2);
-a4=sparse(inputCapa2);
-
-argRelu=sparse(a4*w3); %CONVOLUCION(IMAGEN,FILTRO)
-n2=300; %Aumenta los valores para el input
-a3=reLu(argRelu);
-a3=a3*0.03;
-%a3=sparse(n*funcionAct(argRelu));
-imshow(reshape(full(a3),rango,[]));
+a4=reLu(sparse(a5*w4));
+a3=0.03*reLu(sparse(a4*w3));
 
 %POOLING
 
@@ -199,15 +220,29 @@ while(i<(rango-1)*(rango))
 end
 vecIndexIP=1:con;
 %imshow(reshape(2*vecValueP,sqrt(rango*rango/4),[]));
-a2=sparse(2*vecValuePx);
+a2=sparse(vecValuePx);
+
+z=load('w1.txt');
+
+row = z(:,1);
+col = z(:,2);
+v = z(:,3);
+w1 = sparse(row, col, v,3844,2);
+
+a1=sparse(reLu(a2*w1));
+
+
+
+
+
 %POOLING
 
 
 
 %FULLY CONNECTED NN LAYER
-[x,y]=size(a2);
-w1=sparse(randi([-3,3],y,2));
-a1=sparse(reLu(a2*w1));
+% [x,y]=size(a2);
+% w1=sparse(randi([-3,3],y,2));
+% a1=sparse(reLu(a2*w1));
 eo=[0,1];
 lr=0.01;
 % w3=w3 - lr*(a4'*a3);
@@ -220,7 +255,11 @@ lr=0.01;
 az=a1;
 b=0;
 tic
-while(dot(eo-a1,eo-a1)/2>0.001)
+
+
+
+
+while(dot(eo-a1,eo-a1)/2>=0.2)
     b=b+1
     w1=w1 - lr*sparse(a2'*(a1-eo));
 
